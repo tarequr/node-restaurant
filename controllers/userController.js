d442ce4cb52a286d33bac8c3accbd747b45e1a2f
tarequr <tarequrr8@gmail.com>
2024-07-28 22:32:10 +0600

    Implement GET user request to retrieve user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,35 @@ const JWT = require('jsonwebtoken');
 
 /* GET USER CONTROLLER */
 const getUserController = async(req, res) => {
-    res.status(200).send("User Data");
+    try {
+        /* Find User */
+        const user = await userModel.findById({ _id:req.body.id }, { _id:0 });
+
+        /* Validation */
+        if (!user) {
+            return res.status(404).send({ 
+                success: false,
+                message: 'User Not Found'
+            });
+        }
+
+        /* Hide User Password */
+        user.password = undefined;
+
+        /* Response */
+        res.status(200).send({
+            success: true,
+            message: 'User get successfully',
+            user
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: `Error in Get User API: ${error.message}`,
+            error
+        });
+    }
 }
 
 module.exports = { getUserController }
