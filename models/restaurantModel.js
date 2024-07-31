76402401b6ae0093c5a79a398d87cce39caa3ae9
tarequr <tarequrr8@gmail.com>
2024-07-31 21:19:37 +0600

    Add restaurant model

---
+++ models/restaurantModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+//Schema
+const restaurantSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'The title field is required']
+    },
+    imageUrl: {
+        type: String
+    },
+    foods: {
+        type: Array
+    },
+    time: {
+        type: String
+    },
+    pickup: {
+        type: Boolean,
+        default: true
+    },
+    delivery: {
+        type: Boolean,
+        default: true
+    },
+    isOpen: {
+        type: Boolean,
+        default: true
+    },
+    logoUrl: {
+        type: String,
+        default: true
+    },
+    rating: {
+        type: Number,
+        default: 1,
+        min: 1,
+        max: 5
+    },
+    ratingCount: {
+        type: String
+    },
+    code: {
+        type: String
+    },
+    coords: {
+        id: { type: String },
+        title: { type: String },
+        latitude: { type: Number },
+        latitudeDelta: { type: Number },
+        longitude: { type: Number },
+        longitudeDelta: { type: Number },
+        address: { type: String }
+    }
+},{timestamps: true});
+
+// Export
+module.exports = mongoose.model('Restaurant', restaurantSchema);
